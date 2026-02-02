30ca838148686d1173ec540b60de9e89e17f5b37
Thomas Kelly <tck227@lehigh.edu>
2026-02-02 16:27:46 -0500

    initial Next.js and Typescript scaffold

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main style={{ maxWidth: 800, margin: "40px auto" }}>
+      <h1>AI Chat</h1>
+    </main>
+  );
+}
